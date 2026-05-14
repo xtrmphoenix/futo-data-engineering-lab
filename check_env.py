1f9b717e298e100a244d6d3aed7443a1e65668c9
xtrmphoenix <benedictsobiere@gmail.com>
2026-05-14 12:20:19 +0000

    environment added and pandas istalled

---
+++ check_env.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import sys
+
+print(f"Python Version: {sys.version}")
+print(f"Python Version: {pd.__version__}")
+print("Cloud Environment Is Ready For Data Engineering!")
+
---
+++ hello_data.py
@@ -0,0 +1,1 @@
+print("Ready For Data Engineering")
